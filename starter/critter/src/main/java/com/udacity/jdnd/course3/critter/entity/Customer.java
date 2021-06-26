9fc0ebf8a9d5bf1fcc99afa2a4cec9e4c406df68
Kevin <kevin7571@gmail.com>
2021-06-25 21:56:20 -0400

    Updates to support id generation per table.

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/entity/Customer.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/entity/Customer.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Entity
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
